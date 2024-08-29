9655b8af09cb435346c33a6eaf20499736d71be7
StopSoo <jpy1030@naver.com>
2024-08-29 17:46:26 +0900

    <add>: section03 chapter05

--- onebite-react/section03/src/index.js
+++ onebite-react/section03/src/index.js
@@ -16,3 +16,11 @@ console.log(sub(2, 1));
 import multiply, { add, sub } from "./math.js";
 console.log(add(1, 2));
 console.log(sub(5, 3));
+
+// npm i randomcolor
+// package-lock.json : package.json 파일보다 더 구체적으로 작성됨.
+// node_modules 파일 안에 다운 받은 라이브러리 파일이 생성됨
+// npm에서 다운로드 받은 라이브러리 사용 예시
+import randomColor from "randomcolor";  // 라이브러리 import - 라이브러리의 이름만 명시.
+const color = randomColor();
+console.log(color); // 랜덤으로 색깔 코드가 출력됨
