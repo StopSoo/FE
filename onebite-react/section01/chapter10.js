327f7154f948452fbe7942e77623cee458aeed6d
StopSoo <jpy1030@naver.com>
2024-08-30 16:16:15 +0900

    <docs>: chapter10 재수강

--- onebite-react/section01/chapter10.js
+++ onebite-react/section01/chapter10.js
@@ -1,7 +1,7 @@
 // for문
-// 초기식; 조건식; 증감식
+// for (초기식; 조건식; 증감식)
 for (let idx=0; idx < 5; idx++) {
-  if (idx >= 5) break;
-  if (idx % 2 == 0) continue;
+  if (idx >= 5) break;  // 반복문 중단하기
+  if (idx % 2 == 0) continue; // 건너뛰기
   console.log(idx);
 }
