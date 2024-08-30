80c90b2db0c27be0e065283f9f135203b98195db
StopSoo <jpy1030@naver.com>
2024-08-30 16:08:00 +0900

    <docs>: chapter09 재수강

--- onebite-react/section01/chapter09.js
+++ onebite-react/section01/chapter09.js
@@ -1,7 +1,6 @@
 // 1. if 조건문 (if문)
 // if - else if - else
 // 조건을 괄호 안에 넣기 !
-
 let num = 10;
 
 if (num >= 10) {
@@ -13,7 +12,6 @@ if (num >= 10) {
 // 2. switch문
 // if문과 기능 자체는 동일하나, 다수의 조건을 처리할 때 if보다 더 직관적.
 // 포인트 1) 괄호 사용 2) 콜론과 중괄호 3) break문 4) default문
-
 let animal = "cat";
 
 switch (animal) {
@@ -31,6 +29,10 @@ switch (animal) {
   }
   case "tiger": {
     console.log("호랑이");
+    break;
+  }
+  case "snake": {
+    console.log("뱀");
     break;
   }
   default: {
