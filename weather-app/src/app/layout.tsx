94e609e2dd3079f49f8415761b6fd48cedc1b701
StopSoo <jpy1030@naver.com>
2024-09-06 17:00:45 +0900

    <add>: 지역적/전역적 스타일링 적용

--- weather-app/src/app/layout.tsx
+++ weather-app/src/app/layout.tsx
@@ -1,4 +1,8 @@
+import './global.css';
 import type { Metadata } from "next";
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         {children}
       </body>
     </html>
--- weather-app/src/app/page.tsx
+++ weather-app/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import style from './style.module.css';
 
 export default function Home() {
   return (
     <>
       <h1>main</h1>
-      <ul>
+      <ul className={style.list}>
         <li>
           <Link href="/seoul">서울</Link>
         </li>
@@ -15,6 +16,7 @@ export default function Home() {
           <Link href="/london">런던</Link>
         </li>
       </ul>
+      <button>버튼</button>
     </>
   );
 }
