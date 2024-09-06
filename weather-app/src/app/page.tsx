cfc0ca91fbede13311d1af2d082f3d0f62777f13
StopSoo <jpy1030@naver.com>
2024-09-06 17:32:59 +0900

    <add>: 환경변수 설정을 통한 API 호출

--- weather-app/src/app/page.tsx
+++ weather-app/src/app/page.tsx
@@ -1,7 +1,24 @@
 import Link from "next/link";
 import style from './style.module.css';
 
-export default function Home() {
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY; // API를 동적으로 받아오기 위함.
+
+const getCurrentWeather = async () => { //eslint-disable-line no-unused-vars
+  const res = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Seoul&aqi=no`
+  );
+
+  if (!res.ok) {
+    throw new Error('날씨 정보를 가져올 수 없습니다.');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const res = await getCurrentWeather();
+  console.log(res);
+
   return (
     <>
       <h1>main</h1>
