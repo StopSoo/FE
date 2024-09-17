a8f3547480133e80abaf57e571bbf9f6c52019fe
StopSoo <jpy1030@naver.com>
2024-09-17 23:53:44 +0900

    <add>: 페이지별 레이아웃 적용

--- onebite-next/section02/src/pages/_app.tsx
+++ onebite-next/section02/src/pages/_app.tsx
@@ -1,12 +1,27 @@
 // 글로벌 레이아웃을 설정하는 파일
 import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { ReactNode } from "react";
+// 새로운 타입 정의
+// &: 교집합
+// getLayout을 option type으로 선언.
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+}
+// Component의 타입을 확장시킴.
+export default function App({ Component, pageProps }: AppProps & {
+  Component: NextPageWithLayout;
+}) {
+  // 1. 객체에 선언한 메서드를 다음과 같이 불러와서 호출하여 반환.
+  // 2. 다음 메서드를 사용하지 않는 페이지들을 위한 예외 처리 적용.
+  // 3. getLayout의 적절한 타입 선언을 위해 위 절차들을 거침.
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
 
-export default function App({ Component, pageProps }: AppProps) {
   return (
     <GlobalLayout>
-      <Component {...pageProps} />  
+      {getLayout(<Component {...pageProps} />)}
     </GlobalLayout>
   );
 }
--- onebite-next/section02/src/pages/index.tsx
+++ onebite-next/section02/src/pages/index.tsx
@@ -1,8 +1,16 @@
 // CSS module
+import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
+import { ReactNode } from "react";
 
 export default function Home() {
   return (
     <h1 className={style.h1}>인덱스</h1>
   );
+}
+// 메서드 추가
+// Next.js에서는 모든 페이지가 React Component!
+// 페이지를 받아와서 SearchableLayout 컴포넌트로 감싸진 페이지를 return.
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
 }
