4f313af66d262a0ecb3a6fe1c0a94b456b8d3d55
StopSoo <jpy1030@naver.com>
2024-09-17 23:53:44 +0900

    <add>: 페이지별 레이아웃 적용

--- onebite-next/section02/src/pages/search.tsx
+++ onebite-next/section02/src/pages/search.tsx
@@ -1,4 +1,6 @@
+import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
+import { ReactNode } from "react";
 
 export default function Page() {
   const router = useRouter();
@@ -6,4 +8,8 @@ export default function Page() {
   const { q } = router.query; // 구조분해할당 문법을 이용해서 쿼리스트링을 가져옴.
 
   return (<h1>Search {q}</h1>);
+}
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
 }
