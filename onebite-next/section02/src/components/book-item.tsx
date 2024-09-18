afca824112dd84a183e765e2f202252efc3ba19c
StopSoo <jpy1030@naver.com>
2024-09-18 16:37:54 +0900

    <add>: 메인 페이지 UI 구현

---
+++ onebite-next/section02/src/components/book-item.tsx
@@ -0,0 +1,27 @@
+import type { BookData } from "@/types";
+import Link from "next/link";
+import style from './book-item.module.css';
+// 책 컴포넌트 클릭 시 해당 책 상세 페이지로 이동.
+export default function BookItem({
+  id,
+  title,
+  subTitle,
+  description,
+  author,
+  publisher,
+  coverImgUrl,
+}: BookData) {
+  return (
+    <Link href={`/book/${id}`} className={style.container}>
+      <img src={coverImgUrl}/>
+      <div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>: {subTitle}</div>
+        <br />
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+      </div>
+    </Link>
+  );
+}
