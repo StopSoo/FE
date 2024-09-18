768298240f3f36c847156cc83aa8f44beb117a41
StopSoo <jpy1030@naver.com>
2024-09-18 14:15:59 +0900

    <ans>: Day04 답안 수정

--- onebite-next/onebite-cinema/src/pages/components/global-layout.tsx
+++ onebite-next/onebite-cinema/src/pages/components/global-layout.tsx
@@ -10,7 +10,7 @@ export default function GlobalLayout({ children }: {
       <header className={style.header}>
         <Link href={'/'}>ONEBITE-CINEMA</Link>
       </header>
-      <main>{children}</main>
+      {children}
       <footer></footer>
     </div>
   );
