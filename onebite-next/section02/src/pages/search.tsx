a6f44f6d887666cdd39538b5252d89e9d57247bb
StopSoo <jpy1030@naver.com>
2024-09-18 17:23:29 +0900

    <add>: 책 상세 정보 페이지 UI 구현

--- onebite-next/section02/src/pages/search.tsx
+++ onebite-next/section02/src/pages/search.tsx
@@ -1,13 +1,15 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from '@/mock/books.json';
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-  const router = useRouter();
-
-  const { q } = router.query; // 구조분해할당 문법을 이용해서 쿼리스트링을 가져옴.
-
-  return (<h1>Search {q}</h1>);
+  return (
+    <div>
+      {books.map((book) => <BookItem key={book.id} {...book} />)}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
