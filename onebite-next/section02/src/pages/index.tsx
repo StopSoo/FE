4b308b9275cef52ef74b79c7623e73c8702f8110
StopSoo <jpy1030@naver.com>
2024-09-20 21:37:43 +0900

    <add>: SSR 실습1

--- onebite-next/section02/src/pages/index.tsx
+++ onebite-next/section02/src/pages/index.tsx
@@ -4,8 +4,23 @@ import style from "./index.module.css";
 import { ReactNode } from "react";
 import books from '@/mock/books.json';
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
+// 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
+export const getServerSideProps = () => {
+  const data = "hello";
 
-export default function Home() {
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({ 
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
+
   return (
     <div className={style.container}>
       <section>
