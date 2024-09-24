3ff2966cdf9a909c89c7ee0b8025144c86d71695
StopSoo <jpy1030@naver.com>
2024-09-25 00:36:08 +0900

    <add>: 도서 관련 api 연결

--- onebite-next/section02/src/lib/fetch-random-books.ts
+++ onebite-next/section02/src/lib/fetch-random-books.ts
@@ -1,11 +1,12 @@
 import { BookData } from "@/types";
 
-export default async function fetchBooks(): Promise<BookData[]> {
-  const url = `http://localhost:12345/book`;
-
+export default async function fetchRandomBooks(): Promise<BookData[]> {
+  const url = `http://localhost:12345/book/random`;
+  
   try {
     const response = await fetch(url);
-    if (!response.ok) 
+
+    if (!response.ok)
       throw new Error();
 
     return await response.json();
