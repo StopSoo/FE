3247577741fd3ee3ced8bcb5824bf9666891ed97
StopSoo <jpy1030@naver.com>
2024-09-24 23:54:39 +0900

    <add>: book type 정의

--- onebite-next/section02/src/pages/index.tsx
+++ onebite-next/section02/src/pages/index.tsx
@@ -7,20 +7,17 @@ import BookItem from "@/components/book-item";
 import { InferGetServerSidePropsType } from "next";
 // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
 export const getServerSideProps = () => {
-  const data = "hello";
-
   return {
     props: {
-      data,
+
     },
   };
 };
 
 export default function Home({ 
-  data,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(data);
 
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  
   return (
     <div className={style.container}>
       <section>
