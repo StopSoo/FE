7eb1baa62040640077fe90cdfbb0419190d7e7c7
StopSoo <jpy1030@naver.com>
2024-09-30 15:47:14 +0900

    <build>: API 연결 수정 및 재배포

--- onebite-next/section02/src/lib/fetch-books.ts
+++ onebite-next/section02/src/lib/fetch-books.ts
@@ -2,7 +2,7 @@ import { BookData } from "@/types";
 // q: 검색어라서, optional 키워드로 설정.
 export default async function fetchBooks(q?: string): Promise<BookData[]> {
   // 검색 결과 호출을 적용하기 위해 url을 let 키워드로 설정(!)
-  let url = `http://localhost:12345/book`;
+  let url = `https://onebite-books-server-snowy.vercel.app/book`;
   // 검색어 존재 시 url을 해당 키워드에 맞게 변경
   if (q) { 
     url += `/search?q=${q}`;
