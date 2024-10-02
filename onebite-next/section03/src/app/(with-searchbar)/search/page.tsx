2626df6721f4cb267158760a5c114a9e9911fc84
StopSoo <jpy1030@naver.com>
2024-10-03 00:04:55 +0900

    <add>: Link 컴포넌트를 통한 페이지 네비게이팅

--- onebite-next/section03/src/app/(with-searchbar)/search/page.tsx
+++ onebite-next/section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,3 +1,5 @@
+import ClientComponent from "@/components/client-component";
+
 // props의 형태를 먼저 확인한 후 타입 설정.
 export default function Page({
   searchParams 
@@ -7,6 +9,9 @@ export default function Page({
   return (
     <div>
       Search Page {searchParams.q}
+      <ClientComponent>
+        <></>
+      </ClientComponent>
     </div>
   );
 }
