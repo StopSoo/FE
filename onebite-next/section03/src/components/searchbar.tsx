f74d1df768ab6de433a50c97f73ba38ce05d0c89
StopSoo <jpy1030@naver.com>
2024-10-03 00:30:53 +0900

    <add>: 라우터를 통한 페이지 네비게이팅

--- onebite-next/section03/src/components/searchbar.tsx
+++ onebite-next/section03/src/components/searchbar.tsx
@@ -1,11 +1,17 @@
 "use client"; // 검색바의 클라이언트 컴포넌트화
+import { useRouter } from "next/navigation";  // app router
 import { useState } from "react";
 
 export default function Searchbar() {
+  const router = useRouter();
   const [search, setSearch] = useState("");
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    router.push(`/search?q=${search}`);
   };
 
   return (
@@ -14,7 +20,7 @@ export default function Searchbar() {
         value={search} 
         onChange={onChangeSearch}
       />
-      <button>검색</button>
+      <button onClick={onSubmit}>검색</button>
     </div>
   );
 }
