2b6d9ad4139b8261cf0722ff274b7d919186bc07
StopSoo <jpy1030@naver.com>
2024-10-29 16:06:16 +0900

    add: 데이터 캐시

--- onebite-next/section04/src/app/(with-searchbar)/page.tsx
+++ onebite-next/section04/src/app/(with-searchbar)/page.tsx
@@ -3,9 +3,12 @@ import style from "./page.module.css";
 import { BookData } from "@/types";
 // 모든 도서
 async function AllBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "no-store" }
+  );
   if (!response.ok) {
-    return <div id='error'>오류가 발생했습니다.</div>;
+    return <div>오류가 발생했습니다.</div>;
   }
 
   const allBooks: BookData[] = await response.json(); // any 자동 추론 오류를 직접 단언해서 해결.
@@ -20,9 +23,12 @@ async function AllBooks() {
 }
 // 추천 도서
 async function RecoBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } } // 3초마다 데이터 갱신
+  );
   if (!response.ok) {
-    return <div id='error'>오류가 발생했습니다.</div>;
+    return <div>오류가 발생했습니다.</div>;
   }
 
   const recoBooks: BookData[] = await response.json();
