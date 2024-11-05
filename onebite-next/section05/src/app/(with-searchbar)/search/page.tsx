55b4097efaf29589dedfe6388e5e2d156114df21
StopSoo <jpy1030@naver.com>
2024-11-05 17:18:11 +0900

    feat: 동적 경로를 갖는 페이지를 정적 페이지로 설정하기

--- onebite-next/section05/src/app/(with-searchbar)/search/page.tsx
+++ onebite-next/section05/src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,7 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
-
+// 동적 함수를 사용하기 때문에 동적 페이지가 됨.
+// 따라서 풀 라우트 캐시 적용 불가(!)
 export default async function Page({
   searchParams,
 }: {
@@ -8,9 +9,11 @@ export default async function Page({
     q?: string;
   };
 }) {
-  
+  // cache 옵션을 "force-cache"로 설정함.
+  // 페이지는 계속 재생성이 되겠지만, 검색 결과는 캐싱이 이루어져 한 번 검색된 데이터에 대해서는 좀 더 빠른 속도로 응답 가능.
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
+    { cache: "force-cache" }
   );
   // 예외 처리
   if (!response.ok) {
