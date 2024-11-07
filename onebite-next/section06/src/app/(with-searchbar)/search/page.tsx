cebbd99c0b8f274fcba912e63c93640f2c7d8259
StopSoo <jpy1030@naver.com>
2024-11-07 22:44:26 +0900

    add: section06 패키지 생성

---
+++ onebite-next/section06/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,39 @@
+import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
+/** 
+ * 라우트 세그먼트 옵션
+ * - force-static: 쿼리 스트링은 빈 값을 반환하며, 데이터 캐싱도 강제로 실행됨. 검색 결과 반환이 원활히 이루어지지 않음.
+ * - error: force-static과 같은 역할을 하나, 이 페이지에서는 동적 함수를 사용하므로 빌드 오류를 발생시킴.
+ */
+// export const dynamic = "error";
+
+// 동적 함수를 사용하기 때문에 동적 페이지가 됨.
+// 따라서 풀 라우트 캐시 적용 불가(!)
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  // cache 옵션을 "force-cache"로 설정함.
+  // 페이지는 계속 재생성이 되겠지만, 검색 결과는 캐싱이 이루어져 한 번 검색된 데이터에 대해서는 좀 더 빠른 속도로 응답 가능.
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
+    { cache: "force-cache" }
+  );
+  // 예외 처리
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+
+  const books: BookData[] = await response.json();
+
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
