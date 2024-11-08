d2f1b3ad9c2a66a74d4ac972cd84539df47c4e86
StopSoo <jpy1030@naver.com>
2024-11-08 17:36:18 +0900

    add: Suspense를 활용한 스트리밍 적용

--- onebite-next/section06/src/app/(with-searchbar)/page.tsx
+++ onebite-next/section06/src/app/(with-searchbar)/page.tsx
@@ -1,6 +1,8 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
 /** 
  * 라우트 세그먼트 옵션
  * Ex> dynamic: 특정 페이지의 유형을 Static, Dynamic 페이지로 설정.
@@ -9,9 +11,10 @@ import { BookData } from "@/types";
  * 3. force-static: 페이지를 강제로 Static 페이지로 설정. 쿼리 스트링 등의 동적 함수는 빈 값을 반환.
  * 4. error: 페이지를 강제로 Static 페이지로 설정. 하지만 Static 페이지로 설정되면 안되는 페이지의 경우 빌드 오류를 발생시킴.
  */
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic"; // 강제로 dynamic page로 설정됨.
 // 모든 도서
 async function AllBooks() {
+  await delay(1500);  // 1.5초의 딜레이
   // cache 옵션을 "force-cache"로 설정함으로써 Dynamic Page -> Static Page로 변경.
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
@@ -33,6 +36,7 @@ async function AllBooks() {
 }
 // 추천 도서
 async function RecoBooks() {
+  await delay(3000);  // 3초의 딜레이
   // cache 옵션 중 revalidate 옵션은 페이지를 dynamic하게 설정하는 데 관여X, 따라서 옵션 유지.
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
@@ -58,11 +62,15 @@ export default function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks />
+        <Suspense fallback={<div>도서를 불러오는 중 ~</div>}>
+          <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<div>도서를 불러오는 중 ~</div>}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
--- onebite-next/section06/src/app/(with-searchbar)/search/page.tsx
+++ onebite-next/section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,7 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
+import { Suspense } from "react";
 /** 
  * 라우트 세그먼트 옵션
  * - force-static: 쿼리 스트링은 빈 값을 반환하며, 데이터 캐싱도 강제로 실행됨. 검색 결과 반환이 원활히 이루어지지 않음.
@@ -8,20 +9,12 @@ import { delay } from "@/util/delay";
  */
 // export const dynamic = "error";
 
-// 동적 함수를 사용하기 때문에 동적 페이지가 됨.
-// 따라서 풀 라우트 캐시 적용 불가(!)
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: {
-    q?: string;
-  };
-}) {
+async function SearchResult({ q }: { q: string }) {
   await delay(1500);  // 1.5초만큼 딜레이
   // cache 옵션을 "force-cache"로 설정함.
   // 페이지는 계속 재생성이 되겠지만, 검색 결과는 캐싱이 이루어져 한 번 검색된 데이터에 대해서는 좀 더 빠른 속도로 응답 가능.
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
     { cache: "force-cache" }
   );
   // 예외 처리
@@ -37,5 +30,26 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+// 동적 함수를 사용하기 때문에 동적 페이지가 됨.
+// 따라서 풀 라우트 캐시 적용 불가(!)
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  // SearchResult 컴포넌트를 분리함으로써, Page 컴포넌트는 동기적으로 수행됨.
+  // q가 undefined일 수도 있기 때문에 빈 문자열의 유니온 타입으로 넘겨줌.
+  // 스트리밍을 위해 Suspense 객체로 감싸주고, fallback 옵션을 통해 대체UI를 설정해줌.
+  return (
+    <Suspense 
+      key={searchParams.q || ""}
+      fallback={<div>Loading ...</div>}
+    >
+      <SearchResult q={searchParams.q || ""} />
+    </Suspense>
   );
 }
