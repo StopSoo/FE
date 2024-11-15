859063883628db968acf80d6ff15bec551381c96
StopSoo <jpy1030@naver.com>
2024-11-15 11:52:50 +0900

    feat: OrderPage 컴포넌트의 상태 관리

--- react-season2/1-react/src/pages/OrderPage/index.jsx
+++ react-season2/1-react/src/pages/OrderPage/index.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import OrderApi from "shared/api/OrderApi";
 import Card from "../../components/Card";
 import NavBar from "../../components/NavBar";
 import Page from "../../components/Page";
@@ -5,34 +7,45 @@ import Title from "../../components/Title";
 import OrderDeliveryCard from "./OrderDeliveryCard";
 import OrderPayment from "./OrderPaymentCard";
 import OrderStatusCard from "./OrderStatusCard";
-// mock data
-const fakeOrder = {
-  id: "CACDA420",
-  orderDate: "2023. 7. 22. 오전 11:24:46",
-  status: "배달중",
-  name: "짜장면",
-  totalPrice: 7000,
-  paymentMethod: "마이페이",
-  productPrice: 6000,
-  deliveryPrice: 3000,
-  discountPrice: 2000,
-  deliveryAddress: "서울특별시 송파구 잠실동 1번지",
-  deliveryContact: "010-1111-2222",
-  messageToShop: "포크는 주지 마세요",
-  messageToRider: "안전하게 오세요",
-  position: [60, 60]
-};
 
-const OrderPage = () => {
-  return (
-    <div className="OrderPage">
-      <Page header={<Title>주문내역</Title>} footer={<NavBar />}>
-        <OrderStatusCard order={fakeOrder} />
-        <OrderPayment order={fakeOrder} />
-        <OrderDeliveryCard order={fakeOrder} />
-      </Page>
-    </div>
-  );
+class OrderPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      order: null,
+    };
+  }
+
+  async fetch() {
+    try {
+      const order = await OrderApi.fetchMyOrder();
+      this.setState({ order });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  componentDidMount() {
+    this.fetch();
+  }
+  // order 값이 있을 때만 렌더링
+  render() {
+    const { order } = this.state;
+    return (
+      <div className="OrderPage">
+        <Page header={<Title>주문내역</Title>} footer={<NavBar />}>
+          {order && (
+            <>
+              <OrderStatusCard order={order} />
+              <OrderPayment order={order} />
+              <OrderDeliveryCard order={order} />
+            </>
+          )}
+        </Page>
+      </div>
+    );
+  }
 }
 
 export default OrderPage;
