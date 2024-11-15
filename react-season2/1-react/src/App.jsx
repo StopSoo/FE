18fdf71417dc7456148ff5f9dc6a7bac9e6c80e3
StopSoo <jpy1030@naver.com>
2024-11-15 12:10:40 +0900

    feat: CartPage 컴포넌트 틀 구현하기

--- react-season2/1-react/src/App.jsx
+++ react-season2/1-react/src/App.jsx
@@ -1,11 +1,13 @@
 import ProductPage from "./pages/ProductPage/index";
 import OrderPage from "./pages/OrderPage/index";
+import CartPage from "./pages/CartPage";
 
 const App = () => {
   return (
     <>
       {/* <ProductPage /> */}
-      <OrderPage />
+      {/* <OrderPage /> */}
+      <CartPage />
     </>
   );
 };
