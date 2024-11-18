09005828b32362746dac72cfbb2007263993c4d0
StopSoo <jpy1030@naver.com>
2024-11-18 14:31:23 +0900

    feat: ref 객체를 form에 연결하고, 제출된 form value들을 추출하기

--- react-season2/1-react/src/App.jsx
+++ react-season2/1-react/src/App.jsx
@@ -13,3 +13,27 @@ const App = () => {
 };
 
 export default App;
+
+// 1) ref 객체를 통해 DOM에 직접 접근 가능.
+// 2) ref를 어떤 태그에 전달하느냐에 따라 ref객체의 속성이 달라짐.
+// import React from 'react';
+
+// class MyComponent extends React.Component {
+//   divRef = React.createRef();
+
+//   render() {
+//     return <div ref={this.divRef}>div</div>;
+//   }
+
+//   componentDidMount() {
+//     // 실제 DOM에 있는 div element 값이 컴포넌트 마운트 후 current에 들어와 있음을 확인 가능.
+//     console.log(this.divRef);
+
+//     const divElement = this.divRef.current;
+//     divElement.style.backgroundColor = "red";
+//     divElement.style.width = "100px";
+//     divElement.style.height = "100px";
+//   }
+// }
+
+// export default MyComponent;
--- react-season2/1-react/src/pages/CartPage/OrderForm.jsx
+++ react-season2/1-react/src/pages/CartPage/OrderForm.jsx
@@ -1,48 +1,87 @@
+import React from "react";
 import FormControl from "../../components/FormControl";
 // 1. htmlFor값과 input태그의 name값을 같게 해야 레이블 클릭 시 필드에 포커싱 가능(!) + id 설정도 필수.
 // 2. form에 id를 설정하고 이를 button에서 form 속성으로 지정하면, 해당 버튼을 클릭 시 폼이 제출됨(!)
-const OrderForm = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault(); // 리렌더링 방지
-    console.log("submit");
-  };
+// 3. form 객체에 ref 속성 지정 -> 컴포넌트 렌더링, DOM에 마운트 후 this.formRef.current에 form element의 값이 저장됨.
+class OrderForm extends React.Component {
+  // handleSubmit은 비동기로 동작해야 하기 때문에, constructor를 통해 this를 고정시킴.
+  constructor(props) {
+    super(props);
 
-  return (
-    <form className="OrderForm" id="order-form" onSubmit={handleSubmit}>
-      <FormControl label="주소" htmlFor={"deliveryAddress"} required>
-        <input
-          type="text"
-          name="deliveryAddress"
-          id="deliveryAddress"
-          placeholder="배달 받을 주소를 입력하세요"
-          required
-          autoFocus
-        />
-      </FormControl>
-      <FormControl label="연락처" htmlFor={"deliveryContact"} required>
-        <input
-          type="text"
-          name="deliveryContact"
-          id="deliveryContact"
-          placeholder="연락처를 입력하세요"
-          required
-          pattern="^\d{2,3}-\d{3,4}-\d{4}$"
-        />
-      </FormControl>
-      <FormControl label="결제수단" htmlFor={"paymentMethod"} required>
-        <select name="paymentMethod" id="paymentMethod" value="">
-          <option value="마이페이">마이페이</option>
-          <option value="직접 결제">직접 결제</option>
-        </select>
-      </FormControl>
-      <FormControl label="가게 사장님께" htmlFor={"messageToShop"}>
-        <textarea name="messageToShop" id="messageToShop" />
-      </FormControl>
-      <FormControl label="라이더님께" htmlFor={"messageToRider"}>
-        <textarea name="messageToRider" id="messageToRider" />
-      </FormControl>
-    </form>
-  );
-};
+    this.formRef = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  // 사용자가 입력한 값을 받아오는 함수
+  getInputValueByName(name) {
+    if (!this.formRef.current) return;
+
+    const inputElement = this.formRef.current.elements.namedItem(name);
+    
+    if (!inputElement) return '';
+    return inputElement.value;
+  }
+
+  handleSubmit(e) {
+    e.preventDefault(); // 리렌더링 방지
+
+    const deliveryAddress = this.getInputValueByName("deliveryAddress");
+    const deliveryContact = this.getInputValueByName("deliveryContact");
+    const paymentMethod = this.getInputValueByName("paymentMethod");
+    const messageToShop = this.getInputValueByName("messageToShop");
+    const messageToRider = this.getInputValueByName("messageToRider");
+
+    console.log("submit", {
+      deliveryAddress,
+      deliveryContact,
+      paymentMethod,
+      messageToShop,
+      messageToRider
+    });
+  }
+
+  render() {
+    return (
+      <form
+        className="OrderForm"
+        id="order-form"
+        onSubmit={this.handleSubmit}
+        ref={this.formRef}
+      >
+        <FormControl label="주소" htmlFor={"deliveryAddress"} required>
+          <input
+            type="text"
+            name="deliveryAddress"
+            id="deliveryAddress"
+            placeholder="배달 받을 주소를 입력하세요"
+            required
+            autoFocus
+          />
+        </FormControl>
+        <FormControl label="연락처" htmlFor={"deliveryContact"} required>
+          <input
+            type="text"
+            name="deliveryContact"
+            id="deliveryContact"
+            placeholder="연락처를 입력하세요"
+            required
+            pattern="^\d{2,3}-\d{3,4}-\d{4}$"
+          />
+        </FormControl>
+        <FormControl label="결제수단" htmlFor={"paymentMethod"} required>
+          <select name="paymentMethod" id="paymentMethod">
+            <option value="마이페이">마이페이</option>
+            <option value="직접 결제">직접 결제</option>
+          </select>
+        </FormControl>
+        <FormControl label="가게 사장님께" htmlFor={"messageToShop"}>
+          <textarea name="messageToShop" id="messageToShop" />
+        </FormControl>
+        <FormControl label="라이더님께" htmlFor={"messageToRider"}>
+          <textarea name="messageToRider" id="messageToRider" />
+        </FormControl>
+      </form>
+    );
+  }
+}
 
 export default OrderForm;
