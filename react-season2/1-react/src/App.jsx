9f6b204b0ba41b1b103a22f7eb2f631d9ad9ab07
StopSoo <jpy1030@naver.com>
2024-11-19 16:38:35 +0900

    add: Event Emitter 추가

--- react-season2/1-react/src/App.jsx
+++ react-season2/1-react/src/App.jsx
@@ -1,6 +1,7 @@
 import ProductPage from "./pages/ProductPage/index";
 import OrderPage from "./pages/OrderPage/index";
 import CartPage from "./pages/CartPage";
+import createEvenEmitter from "shared/lib/EventEmitter";
 
 const App = () => {
   return (
@@ -13,6 +14,17 @@ const App = () => {
 };
 
 export default App;
+
+// 이벤트 에미터
+const eventEmitter = createEvenEmitter(0);
+const logger = (value) => console.log(value);
+
+eventEmitter.on(logger);
+console.log(eventEmitter.get());  // 0
+eventEmitter.set(1);  // 1
+eventEmitter.set(2);  // 2
+
+setTimeout(() => eventEmitter.set(10), 3000); // 비동기 동작도 가능.
 
 // 1) ref 객체를 통해 DOM에 직접 접근 가능.
 // 2) ref를 어떤 태그에 전달하느냐에 따라 ref객체의 속성이 달라짐.
---
+++ react-season2/shared/lib/EventEmitter.js
@@ -0,0 +1,18 @@
+const createEvenEmitter = (value) => {
+  let handlers = [];
+
+  const on = (handler) => handlers.push(handler);
+  const off = (handler) => {
+    handlers = handlers.filter((h) => h !== handler);
+  };
+
+  const get = () => value;  // closer
+  const set = (newValue) => {
+    value = newValue;
+    handlers.forEach(handler => handler(value));
+  }
+
+  return { on, off, get, set };
+};
+
+export default createEvenEmitter;
