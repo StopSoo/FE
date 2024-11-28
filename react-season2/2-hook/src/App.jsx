708d3e788204b642ebff52d35d9a1fbe7898b187
StopSoo <jpy1030@naver.com>
2024-11-28 15:31:21 +0900

    add: 클래스와 함수 비교하기

--- react-season2/2-hook/src/App.jsx
+++ react-season2/2-hook/src/App.jsx
@@ -1,3 +1,29 @@
 const App = () => <>2-hook</>;
 
 export default App;
+// 클래스
+class Contract {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sign() {
+    const capturedName = this.name;
+    setTimeout(() => console.log("서명인: ", capturedName), 3000);
+  }
+}
+
+// const contract = new Contract("사용자1"); // 인스턴스를 생성
+// contract.sign();
+// contract.name = "사용자2"; // setTimeOut 함수 실행 전에 변수 값이 변경됨
+
+// 함수
+function createContract(name) {
+  const sign = () => {
+    setTimeout(() => console.log("서명인: ", name), 3000);
+  };
+  return { sign };
+}
+
+const contract = createContract("사용자3"); // 클래스와 다르게 변수 값 변경 불가능
+contract.sign();
