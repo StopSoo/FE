f4cfa10302b2dabfc3dc3e051624a3f66f5a7150
StopSoo <jpy1030@naver.com>
2024-12-03 15:03:18 +0900

    feat: 메뉴 목록 화면을 훅을 이용해 재구현

--- react-season2/2-hook/src/App.jsx
+++ react-season2/2-hook/src/App.jsx
@@ -1,60 +1,9 @@
-import React, { useState } from "react";
-import MyReact from "./lib/MyReact";
-// const App = () => <>2-hook</>;
-// export default App;
+import ProductPage from "./pages/ProductPage";
 
-const Counter = () => {
-  MyReact.resetCursor(); // 컴포넌트 렌더링과 동시에 커서를 초기화
+const App = () => (
+  <>
+    <ProductPage />
+  </>
+);
 
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("");
-
-  const handleClick = () => setCount(count + 1);
-
-  const handleChangeName = (e) => setName(e.target.value);
-
-  // useEffect()를 사용함으로써 아무리 무거운 부수효과라도 렌더링 후 실행되는 것을 보장.
-  MyReact.useEffect(() => {
-    document.title = `count: ${count} / name: ${name}`; // DOM API 직접 호출 -> 느린 렌더링.
-    console.log("effect 1");
-
-    return function cleanup() {
-      document.title = ""; // 원래대로 바꿔놓을 목적
-      console.log("effect 1 cleanup");
-    };
-  }, [count, name]);
-
-  MyReact.useEffect(() => {
-    localStorage.setItem("name", name);
-    console.log("effect 2");
-  }, [name]);
-  // 초기에 한 번만 실행되도록 하기 위해, 의존성 인자로 빈 배열을 전달.
-  MyReact.useEffect(() => {
-    setName(localStorage.getItem("name") || "");
-  }, []);
-
-  return (
-    <>
-      <button onClick={handleClick}>더하기</button>
-      <input value={name} onChange={handleChangeName} />
-    </>
-  );
-};
-
-export default () => {
-  const [mounted, setMounted] = useState(false);
-
-  const handleToggle = () => {
-    const nextMounted = !mounted;
-    
-    if (!nextMounted) MyReact.cleanupEffects();
-    setMounted(nextMounted);
-  }
-
-  return (
-    <>
-      <button onClick={handleToggle}>Toggle</button>
-      {mounted && <Counter />}
-    </>
-  );
-};
+export default App;
--- react-season2/2-hook/src/pages/ProductPage/index.jsx
+++ react-season2/2-hook/src/pages/ProductPage/index.jsx
@@ -1,3 +1,39 @@
-const ProductPage = () => <>ProductPage</>;
+import { useEffect, useState } from "react";
+import ProductApi from "shared/api/ProductApi";
+import Title from "../../components/Title";
+import Page from "../../components/Page";
+import Navbar from "../../components/NavBar";
+import OrderableProductItem from "./OrderableProductItem";
+
+const ProductPage = () => {
+  const [productList, setProductList] = useState([]);
+
+  const fetch = async () => {
+    try {
+      const productList = await ProductApi.fetchProductList();
+      setProductList(productList);
+    } catch (e) {
+      return;
+    }
+  };
+  // fetch() 함수는 초기에 한 번만 실행되어야 함 => 의존성 인자로 빈 배열 전달.
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  return (
+    <div className="ProductPage">
+      <Page header={<Title>메뉴 목록</Title>} footer={<Navbar />}>
+        <ul>
+          {productList.map((product) => (
+            <li key={product.id}>
+              <OrderableProductItem product={product} />
+            </li>
+          ))}
+        </ul>
+      </Page>
+    </div>
+  );
+};
 
 export default ProductPage;
