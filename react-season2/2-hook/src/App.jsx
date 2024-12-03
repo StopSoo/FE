b42bde8008df52fb808031bdfeef8ffcf3648bc9
StopSoo <jpy1030@naver.com>
2024-12-03 16:01:14 +0900

    feat: 주문 내역 화면을 훅을 이용해 재구현

--- react-season2/2-hook/src/App.jsx
+++ react-season2/2-hook/src/App.jsx
@@ -1,10 +1,11 @@
 import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
-
+import OrderPage from "./pages/OrderPage";
 const App = () => (
   <>
     {/* <ProductPage /> */}
-    <CartPage />
+    {/* <CartPage /> */}
+    <OrderPage />
   </>
 );
 
--- react-season2/2-hook/src/pages/OrderPage/index.jsx
+++ react-season2/2-hook/src/pages/OrderPage/index.jsx
@@ -1,3 +1,41 @@
-const OrderPage = () => <>OrderPage</>;
+import NavBar from "../../components/NavBar";
+import OrderDeliveryCard from "../../pages/OrderPage/OrderDeliveryCard";
+import OrderStatusCard from "../../pages/OrderPage/OrderStatusCard";
+import OrderPayment from "../../pages/OrderPage/OrderPaymentCard";
+import OrderApi from "shared/api/OrderApi";
+import Page from "../../components/Page";
+import Title from "../../components/Title";
+import { useEffect, useState } from "react";
+
+const OrderPage = () => {
+  const [order, setOrder] = useState();
+
+  const fetch = async () => {
+    try {
+      const order = await OrderApi.fetchMyOrder();
+      setOrder(order);
+    } catch (e) {
+      return;
+    }
+  };
+  // fetch() 함수는 초기에 한 번만 실행되어야 함 => 의존성 인자로 빈 배열 전달.
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  return (
+    <div className="OrderPage">
+      <Page header={<Title>주문내역</Title>} footer={<NavBar />}>
+        {order && (
+          <>
+            <OrderStatusCard order={order} />
+            <OrderPayment order={order} />
+            <OrderDeliveryCard order={order} />
+          </>
+        )}
+      </Page>
+    </div>
+  );
+};
 
 export default OrderPage;
