9b08b4c736b3da05184639825eac6c475fed7945
StopSoo <jpy1030@naver.com>
2025-08-06 14:27:58 +0900

    <modifiy>: vite를 통해 react app 생성

--- onebite-react/section05/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import Main from './components/Main.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <App />
-)
