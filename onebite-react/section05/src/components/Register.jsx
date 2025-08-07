13a775572419f8eccc875c9c4dc457be83d271df
StopSoo <jpy1030@naver.com>
2025-08-07 15:13:51 +0900

    <feat>: state로 사용자 입력 관리하기

---
+++ onebite-react/section05/src/components/Register.jsx
@@ -0,0 +1,50 @@
+// 간단한 회원 가입 폼
+// 이름, 생년월일, 국적, 자기소개
+import { useState } from "react";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [birth, setBirth] = useState("");
+  const [country, setCountry] = useState("");
+  const [bio, setBio] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  }
+
+  const onChangeBirth = (e) => {
+    setBirth(e.target.value);
+  }
+
+  const onChangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
+
+  const onChangeBio = (e) => {
+    setBio(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} placeholder={"이름"} />
+      </div>
+      <div>
+        <input value={birth} type="date" onChange={onChangeBirth} />
+      </div>
+      <div>
+        <select value={country} onChange={onChangeCountry}>
+          <option value=""></option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="uk">영국</option>
+        </select>
+      </div>
+      <div>
+        <textarea value={bio} onChange={onChangeBio} placeholder={"자기소개"} />
+      </div>
+    </div>
+  );
+}
+
+export default Register;
