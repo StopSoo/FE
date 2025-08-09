71337d5b8cb49df76b5e5d70b3529b09a149a63b
StopSoo <jpy1030@naver.com>
2025-08-09 15:07:28 +0900

    <feat>: useRef 사용하기

--- onebite-react/section05/src/components/Register.jsx
+++ onebite-react/section05/src/components/Register.jsx
@@ -1,6 +1,6 @@
 // 간단한 회원 가입 폼
 // 이름, 생년월일, 국적, 자기소개
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Register = () => {
   // state 별로 따로 useState 사용했던 것을 하나의 객체로 초기화하는 방식으로 변경 (!)
@@ -10,7 +10,11 @@ const Register = () => {
     country: "",
     bio: "",
   });
+  // 전역변수 사용 대신 useRef() 사용하기
+  const countRef = useRef(0);
+  const inputRef = useRef();
   // 통합 이벤트 핸들러
+  // 객체의 필드에 변수를 사용할 때 JS문법 -> []를 사용하기
   const onChange = (e) => {
     setInput({
       ...input,
@@ -18,13 +22,31 @@ const Register = () => {
     });
   };
 
+  const onSubmit = () => {
+    if (input.name === "") {
+      // 이름을 입력하는 DOM 요소 포커스
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div>
       <div>
-        <input name="name" value={input.name} onChange={onChange} placeholder={"이름"} />
+        <input
+          name="name"
+          ref={inputRef}
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
       </div>
       <div>
-        <input name="birth" value={input.birth} type="date" onChange={onChange} />
+        <input
+          name="birth"
+          value={input.birth}
+          type="date"
+          onChange={onChange}
+        />
       </div>
       <div>
         <select name="country" value={input.country} onChange={onChange}>
@@ -35,10 +57,17 @@ const Register = () => {
         </select>
       </div>
       <div>
-        <textarea name="bio" value={input.bio} onChange={onChange} placeholder={"자기소개"} />
+        <textarea
+          name="bio"
+          value={input.bio}
+          onChange={onChange}
+          placeholder={"자기소개"}
+        />
       </div>
+
+      <button onClick={onSubmit}>제출</button>
     </div>
   );
-}
+};
 
 export default Register;
