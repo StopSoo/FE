ebe4f88d54fe8883114559c44ce99cd766124d66
StopSoo <jpy1030@naver.com>
2025-08-09 14:50:14 +0900

    <refactor>: state와 이벤트 핸들러 하나로 정리하기

--- onebite-react/section05/src/components/Register.jsx
+++ onebite-react/section05/src/components/Register.jsx
@@ -3,37 +3,31 @@
 import { useState } from "react";
 
 const Register = () => {
-  const [name, setName] = useState("");
-  const [birth, setBirth] = useState("");
-  const [country, setCountry] = useState("");
-  const [bio, setBio] = useState("");
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  }
-
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value);
-  }
-
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  };
-
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
+  // state 별로 따로 useState 사용했던 것을 하나의 객체로 초기화하는 방식으로 변경 (!)
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
+  // 통합 이벤트 핸들러
+  const onChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} placeholder={"이름"} />
+        <input name="name" value={input.name} onChange={onChange} placeholder={"이름"} />
       </div>
       <div>
-        <input value={birth} type="date" onChange={onChangeBirth} />
+        <input name="birth" value={input.birth} type="date" onChange={onChange} />
       </div>
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select name="country" value={input.country} onChange={onChange}>
           <option value=""></option>
           <option value="kr">한국</option>
           <option value="us">미국</option>
@@ -41,7 +35,7 @@ const Register = () => {
         </select>
       </div>
       <div>
-        <textarea value={bio} onChange={onChangeBio} placeholder={"자기소개"} />
+        <textarea name="bio" value={input.bio} onChange={onChange} placeholder={"자기소개"} />
       </div>
     </div>
   );
