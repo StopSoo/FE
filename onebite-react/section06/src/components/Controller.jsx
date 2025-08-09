a94dd00a7a48eeb13dc7115600d9b1552eb4103d
StopSoo <jpy1030@naver.com>
2025-08-09 16:09:41 +0900

    <feat>: 카운터 앱 기능 구현하기

--- onebite-react/section06/src/components/Controller.jsx
+++ onebite-react/section06/src/components/Controller.jsx
@@ -1,13 +1,14 @@
-const Controller = () => { 
+// 주의사항: onClick 속성으로 함수를 넘길 때 화살표 함수로 넘겨야 한다(!)
+const Controller = ({ onChangeCount }) => {
   return (
-    <>
-      <button>-1</button>
-      <button>-10</button>
-      <button>-100</button>
-      <button>+100</button>
-      <button>+10</button>
-      <button>+1</button>
-    </>
+    <div>
+      <button onClick={() => onChangeCount(-1)}>-1</button>
+      <button onClick={() => onChangeCount(-10)}>-10</button>
+      <button onClick={() => onChangeCount(-100)}>-100</button>
+      <button onClick={() => onChangeCount(100)}>+100</button>
+      <button onClick={() => onChangeCount(10)}>+10</button>
+      <button onClick={() => onChangeCount(1)}>+1</button>
+    </div>
   );
 };
 
