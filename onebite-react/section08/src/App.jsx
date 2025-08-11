b2e7c11ef7a67d1782abc2421af1c829cf23aade
StopSoo <jpy1030@naver.com>
2025-08-11 21:38:22 +0900

    <feat>: TodoList 페이지 UI 설정

--- onebite-react/section08/src/App.jsx
+++ onebite-react/section08/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Editor from './components/Editor';
+import Header from './components/Header';
+import List from './components/List';
 
 function App() {
   return (
-    <>
-      TODO List
-    </>
+    <div className='App'>
+      <Header />
+      <Editor />
+      <List />
+    </div>
   )
 }
 
