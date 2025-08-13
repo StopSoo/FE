fba5dc8beb2c202da8037118bb02c1f61540b798
StopSoo <jpy1030@naver.com>
2025-08-13 14:17:59 +0900

    <feat>: Todo 프로젝트 렌더링 및 검색 필터링 구현하기

--- onebite-react/section08/src/components/List.jsx
+++ onebite-react/section08/src/components/List.jsx
@@ -1,18 +1,39 @@
+import { useState } from "react";
 import "./List.css";
 import TodoItem from "./TodoItem";
 
 const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    // todo의 content가 검색 키워드를 포함하고 있다면 반환.
+    // content와 search 모두 소문자로 변경해서 비교해야, 대문자/소문자 상관 없이 일치하는지 반환할 수 있음.
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredData = getFilteredData();
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
-      <input placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
       <div className="todos_wrapper">
-        {/* {for (todo in todos) {
-
-        }} */}
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {filteredData.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
       </div>
     </div>
   );
