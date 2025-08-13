5863e724a59ba07fd5f0c36118ee1b31b522ad71
StopSoo <jpy1030@naver.com>
2025-08-13 11:03:43 +0900

    <feat>: Todos 프로젝트 Create 기능 추가하기

--- onebite-react/section08/src/App.jsx
+++ onebite-react/section08/src/App.jsx
@@ -2,15 +2,51 @@ import './App.css'
 import Editor from './components/Editor';
 import Header from './components/Header';
 import List from './components/List';
+import { useRef, useState } from "react";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3); // id reference
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+    // 반드시 setState 함수를 이용해서 값을 변경해야 함(!)
+    setTodos([...todos, newTodo]);
+  };
+
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
-      <Editor />
-      <List />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} />
     </div>
-  )
+  );
 }
 
 export default App;
--- onebite-react/section08/src/components/List.jsx
+++ onebite-react/section08/src/components/List.jsx
@@ -1,12 +1,15 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
 
-const List = () => {
+const List = ({ todos }) => {
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
       <input placeholder="검색어를 입력하세요" />
       <div className="todos_wrapper">
+        {/* {for (todo in todos) {
+
+        }} */}
         <TodoItem />
         <TodoItem />
         <TodoItem />
