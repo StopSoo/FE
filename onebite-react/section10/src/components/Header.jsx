bf59edc6e1edd77c31bf94de37496b9f61f5c781
StopSoo <jpy1030@naver.com>
2025-08-21 15:45:41 +0900

    <refactor>: React.memo와 컴포넌트 렌더링 최적화

--- onebite-react/section10/src/components/Header.jsx
+++ onebite-react/section10/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import "./Header.css";
+import { memo } from "react";
 
 const Header = () => {
   return (
@@ -7,6 +8,6 @@ const Header = () => {
       <h1>{new Date().toDateString()}</h1>
     </div>
   );
-}
-
-export default Header;
+};
+// memo 메서드는 인수로 받은 컴포넌트가 props가 변경되지 않았을 때는 컴포넌트가 리렌더링되지 않도록 함.
+export default memo(Header);
--- onebite-react/section10/src/components/TodoItem.jsx
+++ onebite-react/section10/src/components/TodoItem.jsx
@@ -1,4 +1,5 @@
 import "./TodoItem.css";
+import { memo } from "react";
 
 const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   const onChangeCheckbox = () => {
@@ -18,5 +19,14 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
     </div>
   );
 };
-
-export default TodoItem;
+// 고차 컴포넌트(HOC)
+// memo의 두 번째 인수로 콜백함수를 전달할 수 있다.
+// 콜백 함수의 반환값에 따라 props의 변경 여부를 파악.
+// true면 변경X, false면 변경O.
+export default memo(TodoItem, (prevProps, nextProps) => {
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.isDone !== nextProps.isDone) return false;
+  if (prevProps.date !== nextProps.date) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  return true;
+});
