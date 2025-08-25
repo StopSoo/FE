916cd845eb4232a20467a390f99a7db51a9b1aa8
StopSoo <jpy1030@naver.com>
2025-08-25 11:49:23 +0900

    <feat>: 감정 다이어리 프로젝트에 페이지 이동 적용하기

--- onebite-react/section12/src/App.jsx
+++ onebite-react/section12/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
@@ -11,14 +11,32 @@ import Notfound from "./pages/Notfound";
 
 // 주의사항 1) Routes 컴포넌트 내부에는 Route 컴포넌트만 들어갈 수 있음.
 // 주의사항 2) Routes 컴포넌트 바깥에 컴포넌트를 넣게 되면, 그 컴포넌트가 모든 페이지에 공통으로 들어감.
+
+// 정리 1) 링크가 필요할 때는 Link 컴포넌트 활용하기
+// 정리 2) 이벤트 핸들러 함수에서 페이지 이동이 필요할 때는 useNavigate를 활용하기.
+// 정리 3) Link 컴포넌트와 useNavigate 모두 CSR을 이용한다.
 function App() {
+  const navigate = useNavigate();
+
+  const onClickButton = () => {
+    navigate("/new");
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-      <Route path="*" element={<Notfound />} />
-    </Routes>
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
   );
 }
 
