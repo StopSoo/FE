5b7ff2675d63cf302a8b65f5de26bf1a225a9a13
StopSoo <jpy1030@naver.com>
2025-08-26 14:19:31 +0900

    <design>: 공통 컴포넌트 구현 및 배치

--- onebite-react/section12/src/App.jsx
+++ onebite-react/section12/src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 import { getEmotionImage } from "./util/get-emotion-image";
 // 1. "/": 모든 일기를 조회하는 Home 페이지
@@ -20,25 +22,38 @@ import { getEmotionImage } from "./util/get-emotion-image";
 function App() {
   const navigate = useNavigate();
 
-  const onClickButton = () => {
-    navigate("/new");
-  };
-
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"Right"} />}
+      />
+
+      <Button
+        text={"버튼"}
+        type={"DEFAULT"}
+        onClick={() => {
+          console.log("버튼 클릭!");
+        }}
+      />
+
+      <Button
+        text={"버튼"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("버튼 클릭!");
+        }}
+      />
+
+      <Button
+        text={"버튼"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("버튼 클릭!");
+        }}
+      />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
